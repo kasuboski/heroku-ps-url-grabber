d7d5d7ffac71b3ae74e572c874bffa1ea0f880ab
Josh Kasuboski <josh.kasuboski@gmail.com>
2021-06-29 15:03:54 -0500

    write to a file if env var present

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const fs = require('fs');
+
 const Heroku = require('heroku-client');
 const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN});
 
-
-const app = process.env.HEROKU_APP 
+const filepath = process.env.OUT_FILE;
+const app = process.env.HEROKU_APP;
 heroku.get(`/apps/${app}/config-vars`).then(config => {
     if (config.DATABASE_URL) {
-        console.log(`DATABASE_URL=${config.DATABASE_URL}`);
+        const out = `DATABASE_URL=${config.DATABASE_URL}`;
+        if (filepath && filepath !== '') {
+            fs.writeFileSync(filepath, out);
+        } else {
+            console.log(`DATABASE_URL=${config.DATABASE_URL}`);
+        }
     }
 });
